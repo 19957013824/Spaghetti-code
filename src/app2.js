23408c1b7d821bac8af0c39f1bfc01eddf6a7ed1
shi <1727259503@qq.com>
2020-06-15 17:37:47 +0800

    意大利面代码

--- src/app2.js
+++ src/app2.js
@@ -1,8 +1,24 @@
 import './app2.css'
 import $ from 'jquery'
 
+const html = `
+    <section id="app2">
+        <ol class="tab-bar">
+            <li>1</li>
+            <li>2</li>
+        </ol>
+        <ol class="tab-content">
+            <li>内容1</li>
+            <li>内容2</li>
+        </ol>
+    </section>
+`
+const $element = $(html).appendTo($('body>.page'))
+
 const $taBar = $('#app2 .tab-bar')
 const $tabContent = $('#app2 .tab-content')
+const localkey = 'app2.index'
+const index = localStorage.getItem(localkey) || 0
 
 $taBar.on('click', 'li',e =>{
     const $li = $(e.currentTarget)
@@ -11,10 +27,11 @@ $taBar.on('click', 'li',e =>{
         .siblings()
         .removeClass('selected')
     const $index = $li.index()
+    localStorage.setItem(localkey, $index)
     console.log($index)
     $tabContent.children().eq($index)
                 .addClass('active')
                 .siblings().removeClass('active')
 })
 
-$taBar.children().eq(0).trigger('click')
+$taBar.children().eq(index).trigger('click')
